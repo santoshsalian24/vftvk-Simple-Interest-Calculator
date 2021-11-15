68935e91a2636d6c78ad399e2be1d42b97246bba
santoshsalian24 <94386675+santoshsalian24@users.noreply.github.com>
2021-11-16 00:41:33 +0530

    Final changes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+	console.log("Computing....");
+	p = document.getElementById("principal").value;
+	n = document.getElementById("rate").value;
+	r = document.getElementById("years").value;
+	simpleInterest = (p * n * r) / 100;
+	document.getElementById("result").textContent = simpleInterest;
+	console.log("Computing Done");
 }
-        
